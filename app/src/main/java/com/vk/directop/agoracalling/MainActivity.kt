b603c9de1c917736ab34c468934684a45abcc8b2
Pavel MSI GL62 <fire_ra@bk.ru>
2021-11-17 10:15:22 +0300

    put secret key and go

---
+++ app/src/main/java/com/vk/directop/agoracalling/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.vk.directop.agoracalling
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.vk.directop.agoracalling.ui.theme.AgoraCallingTheme
+
+const val APP_ID = "790542 secret key"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AgoraCallingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    //Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "room_screen"
+                    ) {
+                        composable(route = "room_screen") {
+                            RoomScreen(onNavigate = navController::navigate)
+                        }
+                        composable(
+                            route = "video_screen/{roomName}",
+                            arguments = listOf(
+                                navArgument(name = "roomName") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) {
+                            val roomName = it.arguments?.getString("roomName") ?: return@composable
+                            VideoScreen(
+                                roomName = roomName,
+                                onNavigateUp = navController::navigateUp
+                            )
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AgoraCallingTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/vk/directop/agoracalling/TextFieldState.kt
@@ -0,0 +1,6 @@
+package com.vk.directop.agoracalling
+
+data class TextFieldState(
+    val text: String = "",
+    val error: String? = null
+)
